0d0c56c155afd71753408296e2a077eb1521114c
k168ww <k168ww@gmail.com>
2024-03-20 15:18:11 +0900

    commmm

--- src/com/firebird/test.java
+++ src/com/firebird/test.java
@@ -2,9 +2,7 @@ package com.firebird;
 
 public class test {
     public void test1(){
-        System.out.println("Hello world!");
-
-        System.out.println("push해보기");
+        System.out.println("aaaa");
 
     }
 }
