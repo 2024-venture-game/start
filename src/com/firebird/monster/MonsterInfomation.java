63cb2150efbebd67232c9590454d1224dc7926c7
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2024-03-22 12:59:36 +0900

    배열 시작 변수 변경(0~)

--- src/com/firebird/monster/MonsterInfomation.java
+++ src/com/firebird/monster/MonsterInfomation.java
@@ -4,10 +4,10 @@ public class MonsterInfomation {
 
     public void kindOfMonster(){
 
-        SimpleType[] simpleType = new SimpleType[3];
-        simpleType[1] = new SimpleType("드래곤", 1000, 200, 0, 100);
-        simpleType[2] = new SimpleType("오크", 500, 50, 0, 20);
-        simpleType[3] = new SimpleType("마법사", 100, 300, 0, 5);
+        SimpleMonsterType[] MonsterType = new SimpleMonsterType[3];
+        MonsterType[0] = new SimpleMonsterType("드래곤", 1000, 200, 0, 100);
+        MonsterType[1] = new SimpleMonsterType("오크", 500, 50, 0, 20);
+        MonsterType[2] = new SimpleMonsterType("마법사", 100, 300, 0, 5);
 
     }
 
