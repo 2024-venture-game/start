bf16ab5ee17956ebd14e32c4ff52696d0c862702
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2024-03-22 11:08:40 +0900

    정보를 넣어줄 class 생성

---
+++ src/com/firebird/monster/MonsterInfomation.java
@@ -0,0 +1,14 @@
+package com.firebird.monster;
+
+public class MonsterInfomation {
+
+    public void kindOfMonster(){
+
+        SimpleType[] simpleType = new SimpleType[3];
+        simpleType[1] = new SimpleType("드래곤", 1000, 200, 0, 100);
+        simpleType[2] = new SimpleType("오크", 500, 50, 0, 20);
+        simpleType[3] = new SimpleType("마법사", 100, 300, 0, 5);
+
+    }
+
+}
