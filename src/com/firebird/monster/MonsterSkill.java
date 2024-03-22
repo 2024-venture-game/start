298b8bf6e21f7d21204b8d5a9542b7595cb47c97
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2024-03-22 13:54:43 +0900

    스킬 class 생성(getter/setter 생성)

--- src/com/firebird/monster/MonsterSkill.java
+++ src/com/firebird/monster/MonsterSkill.java
@@ -2,10 +2,38 @@ package com.firebird.monster;
 
 public class MonsterSkill {
 
-    public void DragonSkill(){
+    private String SkillName;       //스킬명
+    private int SkillDamage;        //스킬데미지
 
+    private int SkillHeal;          //스킬 회복
 
-
+    public MonsterSkill(String SkillName, int SkillDamage, int SkillHeal){
+        this.SkillName = SkillName;
+        this.SkillDamage = SkillDamage;
+        this.SkillHeal = SkillHeal;
     }
 
+    public void setSkillName(String Skillname){
+        this.SkillName = Skillname;
+    }
+
+    public String getSkillName(){
+        return SkillName;
+    }
+
+    public void setSkillDamage(int SkillDamage){
+        this.SkillDamage = SkillDamage;
+    }
+
+    public int getSkillDamage(){
+        return SkillDamage;
+    }
+
+    public void setSkillHeal(int SkillHeal){
+        this.SkillHeal = SkillHeal;
+    }
+
+    public int getSkillHeal(){
+        return SkillHeal;
+    }
 }
