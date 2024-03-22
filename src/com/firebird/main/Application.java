c5c91f73efca30705c522f6a019cbcdf5c3e32b3
k168ww <k168ww@gmail.com>
2024-03-22 23:43:13 +0900

    대충 알고리즘

--- src/com/firebird/main/Application.java
+++ src/com/firebird/main/Application.java
@@ -35,11 +35,60 @@ public class Application {
 
         } else {
 
-            System.out.println("다시 선택해주십시오.");
+            System.out.println("다시 숫자를 입력해주십시오.");
 
         }
 
         System.out.println("정말 이 캐릭터로 선택하시겠습니까?");
+        System.out.println("1. 예");
+        System.out.println("2. 아니오");
+        int num2 = sc.nextInt();
+
+        if(num2 == 1) {
+
+            System.out.println("게임을 시작합니다.");
+
+        } else if(num2 == 2) {
+
+            System.out.println("캐릭터를 다시 선택해주십시오.");
+
+        } else {
+
+            System.out.println("다시 숫자를 입력해주십시오.");
+
+        }
+
+
+        while(true) {
+
+            System.out.println("********************");
+            System.out.println("캐릭터 소개");
+
+            System.out.println("전투가 시작되었습니다.");
+            System.out.println("1단계?");
+
+            System.out.println("스킬 번호로 보여주기");
+            System.out.println("사용할 스킬을 선택하시오: ");
+            num = sc.nextInt();
+
+            if(num == 1) {
+
+            }
+
+            if(num == 2) {
+
+            }
+
+            if(num ==3) {
+
+            }
+
+            while(true) {
+
+                System.out.println("공격");
+                System.out.println("성공 여부");
+            }
+        }
 
     }
 
