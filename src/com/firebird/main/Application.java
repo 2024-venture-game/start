51bb5a66e1c8be07146a4e092882169b12d55cb6
k168ww <k168ww@gmail.com>
2024-03-22 14:08:30 +0900

    캐릭터 경우 나누기

--- src/com/firebird/main/Application.java
+++ src/com/firebird/main/Application.java
@@ -7,6 +7,7 @@ public class Application {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+//        Character ch = new Character();
 
         System.out.println("******게임 시작******");
         System.out.println("1. 검사");
@@ -16,6 +17,24 @@ public class Application {
         System.out.println("선택할 캐릭터의 번호를 입력하세요: ");
         int num = sc.nextInt();
 
+        if(num == 1) {
+
+            System.out.println("검사가 선택되었습니다.");
+        } else if(num == 2) {
+
+            System.out.println("마법사가 선택되었습니다.");
+        } else if(num == 3) {
+
+            System.out.println("궁수가 선택되었습니다.");
+        } else if(num == 4) {
+
+            System.out.println("도적이 선택되었습니다.");
+        } else {
+
+            System.out.println("다시 선택해주십시오.");
+        }
+
+        System.out.println("정말 이 캐릭터로 선택하시겠습니까?");
     }
 
 }
