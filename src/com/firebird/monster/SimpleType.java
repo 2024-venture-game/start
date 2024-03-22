cfa14a4f0aeeeb66fc0159767205634076ebfb7d
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2024-03-22 11:08:29 +0900

    기본 몬스터의 정보를 전달할 class 생성(이름, hp, ad, df, lv 등등)

--- src/com/firebird/monster/SimpleType.java
+++ src/com/firebird/monster/SimpleType.java
@@ -1,10 +1,68 @@
 package com.firebird.monster;
 
-public class SimpleType {           //기본 몬스터가 필요한 정보들을 정리해둠
+public class SimpleType {
 
-    public int HP_M;       //체력
-    public int AD_M;          //공격력
-    public int DF_M;         //방어력
-    public int LV_M;        //레벨
+    private String name_M;
+    private int HP_M;       // 체력
+    private int AD_M;          // 공격력
+    private int DF_M;         // 방어력
+    private int LV_M;        // 레벨
 
+
+    // 생성자
+    public SimpleType(String name_M, int HP_M, int AD_M, int DF_M, int LV_M) {
+        this.name_M = name_M;
+        this.HP_M = HP_M;
+        this.AD_M = AD_M;
+        this.DF_M = DF_M;
+        this.LV_M = LV_M;
+    }
+
+    public void setName_M(String name_M){
+        this.name_M = name_M;
+    }
+
+    public String getName_M(){
+        return name_M;
+    }
+
+    // HP_M에 대한 getter
+    public int getHP_M() {
+        return HP_M;
+    }
+
+    // HP_M에 대한 setter
+    public void setHP_M(int HP_M) {
+        this.HP_M = HP_M;
+    }
+
+    // AD_M에 대한 getter
+    public int getAD_M() {
+        return AD_M;
+    }
+
+    // AD_M에 대한 setter
+    public void setAD_M(int AD_M) {
+        this.AD_M = AD_M;
+    }
+
+    // DF_M에 대한 getter
+    public int getDF_M() {
+        return DF_M;
+    }
+
+    // DF_M에 대한 setter
+    public void setDF_M(int DF_M) {
+        this.DF_M = DF_M;
+    }
+
+    // LV_M에 대한 getter
+    public int getLV_M() {
+        return LV_M;
+    }
+
+    // LV_M에 대한 setter
+    public void setLV_M(int LV_M) {
+        this.LV_M = LV_M;
+    }
 }
