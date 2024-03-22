1efe6f1e8d7eb4af70aa1fed8f81536111511b17
minsu <hangukin26@naver.com>
2024-03-22 14:34:38 +0900

    캐릭터 class 생성

---
+++ src/com/firebird/character/character.java
@@ -0,0 +1,13 @@
+package com.firebird.character;
+
+public class character {
+    public int HP_C;
+    public int AD_C;
+    public int DF_C;
+    public int LV_C;
+
+    public character(int HP_C, int AD_C, int DF_C, int LV_C) {
+    }
+}
+
+
