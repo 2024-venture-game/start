2b104c0b4d52594ccce0d361ad68338e9cc9c49d
minsu <hangukin26@naver.com>
2024-03-22 15:52:43 +0900

    캐릭터 선택 class 생성

---
+++ src/com/firebird/character/Archer.java
@@ -0,0 +1,4 @@
+package com.firebird.character;
+
+public class Archer {
+}
--- src/com/firebird/character/character.java
+++ src/com/firebird/character/character.java
@@ -1,10 +1,10 @@
 package com.firebird.character;
 
 public class character {
-    public int HP_C;
-    public int AD_C;
-    public int DF_C;
-    public int LV_C;
+    public int HP_C;        //체력
+    public int AD_C;        //공격력
+    public int DF_C;        //방어력
+    public int LV_C;        //레벨
 
     public character(int HP_C, int AD_C, int DF_C, int LV_C) {
     }
