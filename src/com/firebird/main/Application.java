b37b3bbbd84bc0aade3cfd21966e0727f2df641a
k168ww <k168ww@gmail.com>
2024-03-22 14:01:09 +0900

    게임 시작 + 캐릭터 선택

--- src/com/firebird/main/Application.java
+++ src/com/firebird/main/Application.java
@@ -7,6 +7,15 @@ public class Application {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("******게임 시작******");
+        System.out.println("1. 검사");
+        System.out.println("2. 마법사");
+        System.out.println("3. 궁수");
+        System.out.println("4. 도적");
+        System.out.println("선택할 캐릭터의 번호를 입력하세요: ");
+        int num = sc.nextInt();
+
     }
 
 }
