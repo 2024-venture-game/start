d01d624b427c4bad074975bbcb2784120cf27795
k168ww <k168ww@gmail.com>
2024-03-23 00:31:48 +0900

    전투 시작까지 작성

--- src/com/firebird/main/Application.java
+++ src/com/firebird/main/Application.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 public class Application {
 
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         boolean gameStart = false;
 
         while (!gameStart) {
+            System.out.println("******게임 시작******");
             System.out.println("1. 검사");
             System.out.println("2. 마법사");
             System.out.println("3. 궁수");
             System.out.println("4. 도적");
             System.out.println("선택할 캐릭터의 번호를 입력하세요: ");
-            int num = sc.nextInt();
+            int num = scanner.nextInt();
 
             String characterName = "";
 
@@ -41,10 +41,34 @@ public class Application {
             System.out.println("정말 이 캐릭터로 선택하시겠습니까?");
             System.out.println("1. 예");
             System.out.println("2. 아니오");
-            int num2 = sc.nextInt();
+            int num2 = scanner.nextInt();
 
             if (num2 == 1) {
                 System.out.println("게임을 시작합니다.");
+
+                System.out.println("전투를 시작합니다.");
+
+                while (true) {
+                    System.out.println("전투를 진행하셨나요? (y/n)");
+                    String answer = scanner.next();
+
+                    if (answer.equalsIgnoreCase("y")) {
+                        System.out.println("Game over!");
+                        System.out.println("게임을 종료합니다.");
+                        break;
+                    } else if (answer.equalsIgnoreCase("n")) {
+                        System.out.println("Clear!");
+                        boolean moveOn = askMoveOn(scanner);
+
+                        if (!moveOn) {
+                            System.out.println("게임 끝!");
+                            break;
+                        }
+                    } else {
+                        System.out.println("잘못된 입력입니다. 다시 시도해주세요.");
+                    }
+                }
+
                 gameStart = true; // 게임 시작
             } else if (num2 == 2) {
                 System.out.println("캐릭터를 다시 선택해주십시오.");
@@ -53,38 +77,35 @@ public class Application {
             }
         }
 
-
-//        while(true) {
-//
-//            System.out.println("********************");
-//            System.out.println("캐릭터 소개");
-//
-//            System.out.println("전투가 시작되었습니다.");
-//            System.out.println("1단계?");
-//
-//            System.out.println("스킬 번호로 보여주기");
-//            System.out.println("사용할 스킬을 선택하시오: ");
-//            num = sc.nextInt();
-//
-//            if(num == 1) {
-//
-//            }
-//
-//            if(num == 2) {
-//
-//            }
-//
-//            if(num ==3) {
-//
-//            }
-//
-//            while(true) {
-//
-//                System.out.println("공격");
-//                System.out.println("성공 여부");
-//            }
-//        }
-//
+        scanner.close();
     }
 
+    private static boolean askMoveOn(Scanner scanner) {
+        System.out.println("다음 단계로 이동하시겠습니까? (y/n)");
+        String answer = scanner.next();
+
+        if (answer.equalsIgnoreCase("y")) {
+            startBattle();
+            return true;
+        } else if (answer.equalsIgnoreCase("n")) {
+            System.out.println("게임 포기하시겠습니까? (y/n)");
+            answer = scanner.next();
+            if (answer.equalsIgnoreCase("y")) {
+                System.out.println("게임 종료!");
+                return false;
+            } else if (answer.equalsIgnoreCase("n")) {
+                return askMoveOn(scanner);
+            } else {
+                System.out.println("잘못된 입력입니다. 다시 시도해주세요.");
+                return askMoveOn(scanner);
+            }
+        } else {
+            System.out.println("잘못된 입력입니다. 다시 시도해주세요.");
+            return askMoveOn(scanner);
+        }
+    }
+
+    private static void startBattle() {
+        System.out.println("전투를 시작합니다.");
+    }
 }
