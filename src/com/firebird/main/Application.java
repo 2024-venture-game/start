204722f94cd06a7da5244c5a133cebac230d1940
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2024-03-22 15:34:33 +0900

    스킬 사용해보기 !!삭제 예정!!

--- src/com/firebird/main/Application.java
+++ src/com/firebird/main/Application.java
@@ -2,18 +2,25 @@ package com.firebird.main;
 
 import com.firebird.monster.MonsterInfomation;
 import com.firebird.monster.MonsterSkill;
-import com.firebird.monster.SimpleMonsterType;
+
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
+        MonsterInfomation monsterInformation = new MonsterInfomation();
+        // 몬스터와 스킬 정보 초기화
+        monsterInformation.kindOfMonster();
+        monsterInformation.kindOfSkill();
 
-        MonsterInfomation MI = new MonsterInfomation();
-        SimpleMonsterType SM = new SimpleMonsterType();
-
-        MonsterSkill[] skills = MI.getMonsterSkills();
-
-        if (skills.length > 0) {
-            System.out.println(1000 - skills[1].getSkillDamage());
+        // 스킬 정보 출력
+        MonsterSkill[] skills = monsterInformation.getMonsterSkills();
+        System.out.print("사용할 스킬 번호 입력 : ");
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        if (skills[i] != null) {
+            System.out.println(skills[i - 1].getSkillName() + "로 " + skills[i - 1].getSkillDamage() + "데미지를 주었습니다.");
+        } else {
+            System.out.println("스킬이 null입니다.");
         }
     }
 }
