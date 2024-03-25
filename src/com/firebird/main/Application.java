31d195c83e86eab64abdf24a5e714b3caaa009ef
k168ww <k168ww@gmail.com>
2024-03-25 14:54:03 +0900

    #37 계산 오류 공유

--- src/com/firebird/main/Application.java
+++ src/com/firebird/main/Application.java
@@ -1,5 +1,6 @@
 package com.firebird.main;
 
+import java.util.GregorianCalendar;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,12 +8,21 @@ import com.firebird.character.Archer;
 import com.firebird.character.Bandit;
 import com.firebird.character.Magician;
 import com.firebird.character.Warrior;
+import com.firebird.monster.MonsterInfomation;
+import com.firebird.monster.SimpleMonsterType;
+
+import com.firebird.monster.MonsterInfomation;
+import com.firebird.skill.*;
+import com.sun.tools.javac.Main;
 
 public class Application {
     private static Scanner scanner = new Scanner(System.in);
 
+    public int num;
+
     public static void main(String[] args) {
         boolean gameStart = false;
+
 
         while (!gameStart) {
             System.out.println("******게임 시작******");
@@ -22,6 +32,12 @@ public class Application {
             System.out.println("4. 도적");
             System.out.print("선택할 캐릭터의 번호를 입력하세요: ");
             int num = scanner.nextInt();
+
+
+//            Application gmChar = new Application();
+//            int num3 = gmChar.startBattle(num);
+
+//            startBattle();
 
             String characterName = "";
 
@@ -42,6 +58,7 @@ public class Application {
                     System.out.println("다시 숫자를 입력해주십시오.");
                     continue; // 잘못된 입력 시 다시 반복문의 처음으로 돌아감
             }
+
 
             System.out.println(characterName + "가 선택되었습니다.");
             System.out.println("정말 이 캐릭터로 선택하시겠습니까?");
@@ -143,56 +160,252 @@ public class Application {
         }
     }
 
-    private static void startBattle(int num2) {
+    private static void startBattle(int num) {
         System.out.println("전투를 시작합니다.");
         Random rand = new Random();
-        int monsterHP = rand.nextInt(100) + 1; // 몬스터의 체력을 랜덤으로 설정
+//        int monsterHP = rand.nextInt(100) + 1; // 몬스터의 체력을 랜덤으로 설정
         int playerHP = 100; // 플레이어의 체력 초기화
 
-        while (true) {
-            System.out.println("어떤 스킬을 사용하시겠습니까?");
-            System.out.println("1. 스킬 1");
-            System.out.println("2. 스킬 2");
-            System.out.println("3. 스킬 3");
-            Scanner scanner = new Scanner(System.in);
-            int skillNum = scanner.nextInt();
+        SimpleMonsterType simpleMonsterType = new SimpleMonsterType();
 
-            int damageDealt = 0; // 입힌 데미지 초기화
+        MonsterInfomation monsterInformation = new MonsterInfomation();
 
-            switch (skillNum) {
-                case 1:
-                    damageDealt = 20; // 스킬 1에 대한 데미지 설정
+        monsterInformation.kindOfMonster();
+        monsterInformation.kindOfSkill();
+
+
+        SimpleMonsterType dragon = monsterInformation.getMonsterType()[0]; // 드래곤 불러오기
+        SimpleMonsterType oak = monsterInformation.getMonsterType()[1]; // 오크 불러오기
+        SimpleMonsterType wizard = monsterInformation.getMonsterType()[2]; // 마법사 불러오기
+
+        SkillWarrior warrior = new SkillWarrior();
+        warrior.kindOfCharacterSkill();
+        SkillArcher archer = new SkillArcher();
+        archer.kindOfCharacterSkill();
+        SkillBandit bandit = new SkillBandit();
+        bandit.kindOfCharacterSkill();
+        SkillMagician magician = new SkillMagician();
+        magician.kindOfCharacterSkill();
+
+
+
+        System.out.println("*************************************");
+        System.out.println(wizard.getName_M() + "가 등장했습니다.");
+        System.out.println("전투를 시작합니다.");
+        System.out.println(wizard.getName_M() + "의 현재 체력은 " + wizard.getHP_M() + "입니다.");
+        System.out.println("*************************************");
+
+
+
+
+        if(num == 1){
+            while (true) {
+                CharacterSkill[] skills = warrior.getCharacterSkills();
+
+                System.out.println("어떤 스킬을 사용하시겠습니까?");
+                System.out.println("1." + skills[0].getSkill_name_C() + " 데미지 : " + skills[0].getSkill_damage_C());
+                System.out.println("2." + skills[1].getSkill_name_C() + " 데미지 : " + skills[1].getSkill_damage_C());
+                System.out.println("3." + skills[2].getSkill_name_C() + " 데미지 : " + skills[2].getSkill_damage_C());
+                Scanner scanner = new Scanner(System.in);
+                int skillNum = scanner.nextInt();
+
+                int damageDealt = 0; // 입힌 데미지 초기화
+
+                switch (skillNum) {
+                    case 1:
+                        damageDealt = skills[0].getSkill_damage_C(); // 스킬 1에 대한 데미지 설정
+                        break;
+                    case 2:
+                        damageDealt = skills[1].getSkill_damage_C();; // 스킬 2에 대한 데미지 설정
+                        break;
+                    case 3:
+                        damageDealt = skills[2].getSkill_damage_C();; // 스킬 3에 대한 데미지 설정
+                        break;
+                    default:
+                        System.out.println("잘못된 입력입니다. 다시 시도해주세요.");
+                        continue; // 잘못된 입력 시 다시 반복문의 처음으로 돌아감
+                }
+
+                System.out.println("데미지 확인용 출력문 :" + damageDealt);
+                System.out.println("체력 확인문 : " + wizard.getHP_M());
+
+                simpleMonsterType.setHP_M(wizard.getHP_M() + damageDealt);
+                System.out.println("현재" + wizard.getName_M() + "의 체력은" + wizard.getHP_M() + "입니다.");
+                System.out.println("현재 이 코드가 작동하는지 확인하는 구문");
+
+//            monsterHP -= damageDealt; // 몬스터의 체력 감소
+
+                if (wizard.getHP_M() <= 0) {
+                    System.out.println("몬스터를 물리쳤습니다. 게임을 종료합니다.");
                     break;
-                case 2:
-                    damageDealt = 30; // 스킬 2에 대한 데미지 설정
+                }
+
+                // 몬스터의 공격
+//            int monsterDamage = rand.nextInt(20) + 1; // 몬스터의 데미지를 랜덤으로 설정
+//            playerHP -= monsterDamage; // 플레이어의 체력 감소
+
+                if (playerHP <= 0) {
+                    System.out.println("플레이어가 졌습니다. 게임 끝!");
                     break;
-                case 3:
-                    damageDealt = 40; // 스킬 3에 대한 데미지 설정
-                    break;
-                default:
-                    System.out.println("잘못된 입력입니다. 다시 시도해주세요.");
-                    continue; // 잘못된 입력 시 다시 반복문의 처음으로 돌아감
+                }
+
+//            System.out.println("플레이어 HP: " + playerHP);
+//            System.out.println("몬스터 HP: " + monsterHP);
+
             }
 
-            monsterHP -= damageDealt; // 몬스터의 체력 감소
+        }else if(num == 2){
+            while (true) {
+                System.out.println("어떤 스킬을 사용하시겠습니까?");
+                System.out.println("1. 스킬 1");
+                System.out.println("2. 스킬 2");
+                System.out.println("3. 스킬 3");
+                Scanner scanner = new Scanner(System.in);
+                int skillNum = scanner.nextInt();
 
-            if (monsterHP <= 0) {
-                System.out.println("몬스터를 물리쳤습니다. 게임을 종료합니다.");
-                break;
+                int damageDealt = 0; // 입힌 데미지 초기화
+
+                switch (skillNum) {
+                    case 1:
+                        damageDealt = 20; // 스킬 1에 대한 데미지 설정
+                        break;
+                    case 2:
+                        damageDealt = 30; // 스킬 2에 대한 데미지 설정
+                        break;
+                    case 3:
+                        damageDealt = 40; // 스킬 3에 대한 데미지 설정
+                        break;
+                    default:
+                        System.out.println("잘못된 입력입니다. 다시 시도해주세요.");
+                        continue; // 잘못된 입력 시 다시 반복문의 처음으로 돌아감
+                }
+
+                simpleMonsterType.setHP_M(wizard.getHP_M() - damageDealt);
+                System.out.println("현재" + wizard.getName_M() + "의 체력은" + wizard.getHP_M() + "입니다.");
+                System.out.println("현재 이 코드가 작동하는지 확인하는 구문");
+
+//            monsterHP -= damageDealt; // 몬스터의 체력 감소
+
+                if (wizard.getHP_M() <= 0) {
+                    System.out.println("몬스터를 물리쳤습니다. 게임을 종료합니다.");
+                    break;
+                }
+
+                // 몬스터의 공격
+//            int monsterDamage = rand.nextInt(20) + 1; // 몬스터의 데미지를 랜덤으로 설정
+//            playerHP -= monsterDamage; // 플레이어의 체력 감소
+
+                if (playerHP <= 0) {
+                    System.out.println("플레이어가 졌습니다. 게임 끝!");
+                    break;
+                }
+
+//            System.out.println("플레이어 HP: " + playerHP);
+//            System.out.println("몬스터 HP: " + monsterHP);
+
             }
+        }else if(num == 3){
+            while (true) {
+                System.out.println("어떤 스킬을 사용하시겠습니까?");
+                System.out.println("1. 스킬 1");
+                System.out.println("2. 스킬 2");
+                System.out.println("3. 스킬 3");
+                Scanner scanner = new Scanner(System.in);
+                int skillNum = scanner.nextInt();
 
-            // 몬스터의 공격
-            int monsterDamage = rand.nextInt(20) + 1; // 몬스터의 데미지를 랜덤으로 설정
-            playerHP -= monsterDamage; // 플레이어의 체력 감소
+                int damageDealt = 0; // 입힌 데미지 초기화
 
-            if (playerHP <= 0) {
-                System.out.println("플레이어가 졌습니다. 게임 끝!");
-                break;
+                switch (skillNum) {
+                    case 1:
+                        damageDealt = 20; // 스킬 1에 대한 데미지 설정
+                        break;
+                    case 2:
+                        damageDealt = 30; // 스킬 2에 대한 데미지 설정
+                        break;
+                    case 3:
+                        damageDealt = 40; // 스킬 3에 대한 데미지 설정
+                        break;
+                    default:
+                        System.out.println("잘못된 입력입니다. 다시 시도해주세요.");
+                        continue; // 잘못된 입력 시 다시 반복문의 처음으로 돌아감
+                }
+
+                simpleMonsterType.setHP_M(wizard.getHP_M() - damageDealt);
+                System.out.println("현재" + wizard.getName_M() + "의 체력은" + wizard.getHP_M() + "입니다.");
+                System.out.println("현재 이 코드가 작동하는지 확인하는 구문");
+
+//            monsterHP -= damageDealt; // 몬스터의 체력 감소
+
+                if (wizard.getHP_M() <= 0) {
+                    System.out.println("몬스터를 물리쳤습니다. 게임을 종료합니다.");
+                    break;
+                }
+
+                // 몬스터의 공격
+//            int monsterDamage = rand.nextInt(20) + 1; // 몬스터의 데미지를 랜덤으로 설정
+//            playerHP -= monsterDamage; // 플레이어의 체력 감소
+
+                if (playerHP <= 0) {
+                    System.out.println("플레이어가 졌습니다. 게임 끝!");
+                    break;
+                }
+
+//            System.out.println("플레이어 HP: " + playerHP);
+//            System.out.println("몬스터 HP: " + monsterHP);
+
             }
+        }else if(num == 4){
+            while (true) {
+                System.out.println("어떤 스킬을 사용하시겠습니까?");
+                System.out.println("1. 스킬 1");
+                System.out.println("2. 스킬 2");
+                System.out.println("3. 스킬 3");
+                Scanner scanner = new Scanner(System.in);
+                int skillNum = scanner.nextInt();
 
-            System.out.println("플레이어 HP: " + playerHP);
-            System.out.println("몬스터 HP: " + monsterHP);
+                int damageDealt = 0; // 입힌 데미지 초기화
+
+                switch (skillNum) {
+                    case 1:
+                        damageDealt = 20; // 스킬 1에 대한 데미지 설정
+                        break;
+                    case 2:
+                        damageDealt = 30; // 스킬 2에 대한 데미지 설정
+                        break;
+                    case 3:
+                        damageDealt = 40; // 스킬 3에 대한 데미지 설정
+                        break;
+                    default:
+                        System.out.println("잘못된 입력입니다. 다시 시도해주세요.");
+                        continue; // 잘못된 입력 시 다시 반복문의 처음으로 돌아감
+                }
+
+                simpleMonsterType.setHP_M(wizard.getHP_M() - damageDealt);
+                System.out.println("현재" + wizard.getName_M() + "의 체력은" + wizard.getHP_M() + "입니다.");
+                System.out.println("현재 이 코드가 작동하는지 확인하는 구문");
+
+//            monsterHP -= damageDealt; // 몬스터의 체력 감소
+
+                if (wizard.getHP_M() <= 0) {
+                    System.out.println("몬스터를 물리쳤습니다. 게임을 종료합니다.");
+                    break;
+                }
+
+                // 몬스터의 공격
+//            int monsterDamage = rand.nextInt(20) + 1; // 몬스터의 데미지를 랜덤으로 설정
+//            playerHP -= monsterDamage; // 플레이어의 체력 감소
+
+                if (playerHP <= 0) {
+                    System.out.println("플레이어가 졌습니다. 게임 끝!");
+                    break;
+                }
+
+//            System.out.println("플레이어 HP: " + playerHP);
+//            System.out.println("몬스터 HP: " + monsterHP);
+
+            }
         }
+
     }
 
     private static boolean askMoveOn(Scanner scanner) {
@@ -220,9 +433,6 @@ public class Application {
         }
     }
 }
-
-
-/* 몬스터 설명 불러오기 */
 
 
 
